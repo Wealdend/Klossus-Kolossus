8255cc785241c91a773e0670b7f09554a5bbfdf7
Wealdend <blom.simon95@gmail.com>
2023-03-24 15:48:05 +0100

    simons update
    error BUG

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,15 +1,20 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
     public Tilemap Tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition;
 
     private void Awake()
     {
        this.Tilemap = GetComponentInChildren<Tilemap>();
-        for(int i = 0; i < this.tetrominos.Length; i++)
+       this.activePiece = GetComponentInChildren<Piece>();
+        for (int i = 0; i < this.tetrominos.Length; i++)
+
         {
             this.tetrominos[i].Intialize();
         }
@@ -24,11 +29,18 @@ public class Board : MonoBehaviour
     {
         int random = Random.Range(0, this.tetrominos.Length);
         TetrominoData data = this.tetrominos[random];
+
+        this.activePiece.Intialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece piece)
     {
-
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.Tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 
 }
