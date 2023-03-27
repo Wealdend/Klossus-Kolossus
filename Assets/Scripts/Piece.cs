0faa52ac89a3cfd7a70233979b2684c46601f9d7
sockulags <125367352+sockulags@users.noreply.github.com>
2023-03-27 13:53:16 +0200

    Lucas - Smidigare??? rörelser

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
@@ -38,24 +40,27 @@ public class Piece : MonoBehaviour
         this.lockTime += Time.deltaTime;
 
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.UpArrow) )
         {
             Rotate(-1);
         }
         else if (Input.GetKeyDown(KeyCode.E))
             Rotate(1);
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            Thread.Sleep(100);
             Move(Vector2Int.left);
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            Thread.Sleep(100);
             Move(Vector2Int.right);
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
+            Thread.Sleep(100);
             Move(Vector2Int.down);
         }
 
