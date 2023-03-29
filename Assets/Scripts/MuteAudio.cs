3c7afc35c249ab25d19fa5451eabe0f67f9d66c2
Wealdend <blom.simon95@gmail.com>
2023-03-29 17:31:08 +0200

    muteknapp, audio, slider

--- Assets/Scripts/MuteAudio.cs
+++ Assets/Scripts/MuteAudio.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MuteAudio : MonoBehaviour
 {
-    public void MuteToggle(bool muted)
+    //[SerializeField] Image soundOnIcon;
+    //[SerializeField] Image soundOffIcon;
+    private bool muted = false;
+
+    private void Start()
     {
-        if (muted)
+        if (!PlayerPrefs.HasKey("muted"))
         {
-            AudioListener.volume = 0;
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
         }
         else
         {
-            AudioListener.volume = 1;
+            Load();
         }
+        //UpdateButton();
+        AudioListener.pause = muted;
+    }
+
+    public void OnButtonPress()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+
+        Save();
+    }
+
+    //private void UpdateButton()
+    //{
+    //    if (muted == false)
+    //    {
+    //        soundOnIcon.enabled = true;
+    //        soundOffIcon.enabled = false;
+    //    }
+    //    else
+    //    {
+    //        soundOnIcon.enabled= false;
+    //        soundOffIcon.enabled= true;
+    //    }
+    //}
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
     }
 }
